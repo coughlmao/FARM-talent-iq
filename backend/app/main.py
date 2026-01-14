0f2d265ac88ca9a6acdd2b07ca2e69d5b551e442
Mohit Anand <anandm.mohit@gmail.com>
2026-01-14 16:25:33 +0530

    Use correct PythonSDK for Inngest

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import os
+import inngest.fast_api
 
-from app.lib.inngest import delete_user, sync_user
+from .lib.inngest import inngest_client, sync_user, delete_user
 from .lib.db import connect_db, close_db
 from .routes.chats import router as chatRoutes
 from .lib.config import settings
@@ -28,30 +28,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+inngest.fast_api.serve(
+    app,
+    inngest_client,
+    [sync_user, delete_user],
+    
+    serve_path="/api/inngest"
+)
+
 # API routes FIRST
 app.include_router(chatRoutes, prefix="/api/chats")
 
-# Inngest webhook route
-@app.post("/api/inngest")
-async def inngest_webhook(request: Request):
-    payload = await request.json()
-
-    event_name = payload.get("name")
-
-    if event_name == "clerk/user.created":
-        await sync_user(payload)
-
-    elif event_name == "clerk/user.deleted":
-        await delete_user(payload)
-
-    return {"status": "ok"}
-
-
 # ---------- FRONTEND ----------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_BUILD_PATH = os.path.abspath(
-    os.path.join(BASE_DIR, "../../frontend/dist")
-)
+FRONTEND_BUILD_PATH = os.path.abspath(os.path.join(BASE_DIR, "../../frontend/dist"))
 
 print("FRONTEND_BUILD_PATH =", FRONTEND_BUILD_PATH)
 print("FILES =", os.listdir(FRONTEND_BUILD_PATH))
